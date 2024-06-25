ae65bcd48482f2e53ab391872ceda6643a8796a0
Giorgi Akhobadze <axobadzegiorgi5@gmail.com>
2024-06-25 11:00:13 +0400

    Cards markup and search

---
+++ resources/views/components/job-card-full.blade.php
@@ -0,0 +1,18 @@
+<x-panel class="flex gap-x-6">
+    <div>
+        <x-employer-logo/>
+    </div>
+
+    <div class="flex-1 flex flex-col">
+        <a href="#" class="self-start text-sm text-gray-400">Laracasts</a>
+        <h3 class="font-bold text-xl mt-2 group-hover:text-blue-600 transition-colors duration-200">Video Producer</h3>
+        <p class="text-sm text-gray-500 mt-auto">Full Time - From $60,000</p>
+    </div>
+
+
+    <div class="flex items-start justify-center gap-2">
+        <x-tag>Tag</x-tag>
+        <x-tag>Tag</x-tag>
+        <x-tag>Tag</x-tag>
+    </div>
+</x-panel>
---
+++ resources/views/components/tag.blade.php
@@ -0,0 +1,13 @@
+@props(['size' => 'base'])
+
+@php
+    $class  = "bg-white/10 hover:bg-white/25 rounded-xl font-bold transition-colors duration-300";
+    if($size === 'base') {
+        $class .= " px-5 py-1 text-sm";
+    }
+
+    if($size === 'small') {
+        $class .= " px-3 py-1 text-2xs";
+    }
+@endphp
+<a href="#" class="{{ $class }}">{{ $slot }}</a>
